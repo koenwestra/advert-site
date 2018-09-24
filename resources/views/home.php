68ddec894a2e7a482cf4d509eacea3e950c21289
Koen Westra <koenwestra@gmail.com>
2018-09-24 14:31:42 +0200

    Initial commit

---
+++ resources/views/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<p>Home</p>
+<div class="navbar navbar-static-top navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+      <ul class="nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</body>
+</html>
